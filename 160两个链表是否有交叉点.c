0f5050351934cc63c198d38101735d7640249edc
Micheallering <q2337989150@gmail.com>
2025-07-18 17:28:54 +0800

    second

--- 160两个链表是否有交叉点.c
+++ 160两个链表是否有交叉点.c
@@ -21,22 +21,22 @@ struct ListNode *getIntersectionNode(struct ListNode *headA, struct ListNode *he
     for(;listA->next!=NULL;listA=listA->next)
         distB++;
 
-    //尾指针都不在一起，一定没有交叉点
+    //尾指针都不在一起，一定没有交叉点！
     if(listA!=listB)
         return NULL;
     
     //让更长的那一个链表，先多走多出来的部分
     if(distA > distB){                      
-        for(int i=1 ; i <distA-distB ; i++)
+        for(int i=1 ; i <= distA-distB ; i++)
             headA = headA->next;
     }
     else{
-        for(int i=1 ; i <distB-distA ; i++)
+        for(int i=1 ; i <= distB-distA ; i++)
             headB = headB->next;
     }
 
     //两个链表剩余量一样了，开始一起走，并逐个比对值是否相等
-    while(headA->val != headB->val){
+    while(headA != headB){       //判断是否为交点，必须判断节点地址是否相等（即指针相等 headA == headB），不能只比较值！因为“相交”指的是 两个链表共享节点对象本身（即地址相同），并不是值相同。
         headA = headA->next;
         headB = headB->next;
     }
