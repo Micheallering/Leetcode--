87f02a3429704b7f320c16c244187f7f7d8a9f8d
Micheallering <q2337989150@gmail.com>
2025-07-20 14:48:21 +0800

    second

--- 206反向链表.c
+++ 206反向链表.c
@@ -11,16 +11,15 @@ struct ListNode* reverseList(struct ListNode* head) {
 
     struct ListNode* pre=NULL;
     struct ListNode* cur=head;
-    struct ListNode* back=head->next;
+    struct ListNode* back;      //不能让back直接=head->next。因为在 head != NULL 但 head->next == NULL 时，这句会运行，之后访问 back->next 时，就是访问空指针就会崩溃。
 
     while(cur->next!=NULL){
-        
+        back=cur->next;
         cur->next=pre;
         pre=cur;
         cur=back;
-        back=back->next;
+        // 所以不能back=back->next这么写
     }
 
-    head=cur;
-    return head;
-}
+    
+    return pre;  //pre是新头节点
